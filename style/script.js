ad6fd24ce5fd9325c97a3b657521c6363a9af8d1
Muhammad Shaka Aufa <111035568+saka-C@users.noreply.github.com>
2023-10-09 13:40:34 +0700

    Add files via upload

--- style/script.js
+++ style/script.js
@@ -1,10 +1,14 @@
 const hamburger = document.getElementById("hamburger");
 const view = document.getElementById("view-ticket");
+const report = document.getElementById("report-popup");
+
 const closeBtn = document.getElementById("close-view-popup");
+
 const sidebar = document.querySelector(".sidebar");
 const hamburgerBtn = document.querySelector(".hamburger");
 const container = document.querySelector(".container");
 const viewPopup = document.querySelector(".view-popup");
+const reportPopup = document.querySelector(".report-card-popup");
 
 hamburger.addEventListener("click", () => {
     sidebar.classList.toggle("active");
@@ -16,9 +20,16 @@ hamburger.addEventListener("click", () => {
     container.classList.toggle("active");
 });
 
+
+
 view.addEventListener("click", () => {
     viewPopup.classList.add("active");
 });
+Report.addEventListener("click", () => {
+    reportPopup.classList.toggle("active");
+});
+
+
 
 closeBtn.addEventListener("click",() =>{
     viewPopup.classList.remove("active");
